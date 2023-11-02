6e6ab62e73cc6089060eda4f93f1ac15dd7f6009
jodhyyy <jodhyh71@gmail.com>
2023-11-03 00:14:30 +0700

    progress home

--- app/src/main/java/com/example/massive/Dashboard.kt
+++ app/src/main/java/com/example/massive/Dashboard.kt
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment
 import com.example.massive.databinding.ActivityDashboardBinding
 
 class Dashboard : AppCompatActivity() {
+
     private lateinit var binding: ActivityDashboardBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
