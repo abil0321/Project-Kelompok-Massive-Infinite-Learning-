925e50666d811c953f0da075fc52d6cd89d236e4
jodhyyy <jodhyh71@gmail.com>
2023-11-13 14:41:38 +0700

    Update NavBottom

--- app/src/main/java/com/example/massive/Dashboard.kt
+++ app/src/main/java/com/example/massive/Dashboard.kt
@@ -2,58 +2,33 @@ package com.example.massive
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.widget.Toolbar
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
 import com.example.massive.databinding.ActivityDashboardBinding
-import com.google.android.material.navigation.NavigationView
 
-class Dashboard : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class Dashboard : AppCompatActivity() {
 
-    private lateinit var drawerLayout: DrawerLayout
-    private lateinit var toggle: ActionBarDrawerToggle
     private lateinit var binding: ActivityDashboardBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        replaceFragment(Home())
 
-        drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
-
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
-        val navigationView = findViewById<NavigationView>(R.id.nav_view)
-        navigationView.setNavigationItemSelectedListener(this)
-
-        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        if (savedInstanceState == null){
-            replaceFragment(HomeFragment())
-            navigationView.setCheckedItem(R.id.nav_home)
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(Home())
+                R.id.nutrisi -> replaceFragment(Nutrisi())
+                R.id.reminder -> replaceFragment(Reminder())
+                else ->{
+                }
+            }
+            true
         }
     }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.nav_home -> replaceFragment(HomeFragment())
-            R.id.nav_logout -> Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show()
-        }
-        drawerLayout.closeDrawer(GravityCompat.START)
-        return true
-    }
-
-    private fun replaceFragment(fragment: Fragment){
-        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_container, fragment)
-        transaction.commit()
+    private fun replaceFragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
