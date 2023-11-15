df02b8185f5919967f8da28f6183f04c5581b3a1
jodhyyy <jodhyh71@gmail.com>
2023-11-16 04:36:59 +0700

    Set Reminder

--- app/src/main/java/com/example/massive/Reminder.kt
+++ app/src/main/java/com/example/massive/Reminder.kt
@@ -1,19 +1,80 @@
 package com.example.massive
 
+import android.app.DatePickerDialog
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.massive.databinding.FragmentReminderBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
-class Reminder : Fragment() {
+class Reminder : Fragment(R.layout.fragment_reminder) {
+    private var _binding: FragmentReminderBinding? = null
+    private val binding get() = _binding!!
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentReminderBinding.bind(view)
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reminder, container, false)
+        binding.apply {
+            setReminder.setOnClickListener {
+                showDatePicker()
+            }
+        }
+
+        // Set up the result listener outside of the button click event
+        setupResultListener()
+    }
+
+    private fun showDatePicker() {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            requireContext(),
+            { _, selectedYear, selectedMonth, selectedDay ->
+                val selectedDate = formatDate(selectedYear, selectedMonth, selectedDay)
+                updateSelectedDate(selectedDate)
+            },
+            year,
+            month,
+            day
+        )
+
+        datePickerDialog.show()
+    }
+
+    private fun formatDate(year: Int, month: Int, day: Int): String {
+        val selectedDate = Calendar.getInstance().apply {
+            set(year, month, day)
+        }
+        val dateFormat = SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH)
+        return dateFormat.format(selectedDate.time)
+    }
+
+    private fun updateSelectedDate(date: String) {
+        binding.tvSelectedDate.text = date
+    }
+
+    private fun setupResultListener() {
+        val supportFragmentManager = activity?.supportFragmentManager
+
+        supportFragmentManager?.setFragmentResultListener(
+            "REQUEST_DATE",
+            viewLifecycleOwner
+        ) { resultKey, bundle ->
+            if (resultKey == "REQUEST_DATE") {
+                val date = bundle.getString("SELECTED_DATE")
+                binding.tvSelectedDate.text = date
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
