6a73918bf9ae0847416d2a4887b611ff3884b946
jodhyyy <jodhyh71@gmail.com>
2023-11-20 15:46:57 +0700

    pending

--- app/src/main/java/com/example/massive/EditProfile.kt
+++ app/src/main/java/com/example/massive/EditProfile.kt
@@ -1,22 +1,22 @@
 package com.example.massive
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.massive.databinding.ActivityEditProfileBinding
 
 class EditProfile : AppCompatActivity() {
     private lateinit var binding: ActivityEditProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnDone.setOnClickListener {
-            startActivity(Intent(this, Setting::class.java))
-        }
-        binding.back.setOnClickListener {
-            startActivity(Intent(this, Setting::class.java))
+            val setting = Setting()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.frame_layout, setting)
+                .commit()
         }
     }
 }
